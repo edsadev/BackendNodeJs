260cd771d340ac73a6e8fa408d22eb7b9fdd4550
Edmundo0994 <esala.094@gmail.com>
2020-07-21 12:02:52 -0500

    Clase de uso de modulos os y fs(enfocandose solo en leer un archivo)

---
+++ learningNode/os-archivos/os.js
@@ -0,0 +1,8 @@
+const os = require('os')
+
+// console.log('CPU info ', os.cpus())
+// console.log('IP address ', os.networkInterfaces()['Wi-Fi'].map(i => i.address))
+// console.log('Free Memory ', os.freemem())
+// console.log('OS type: ', os.type())
+// console.log('OS version: ', os.release())
+// console.log('Usr info: ', os.userInfo())
